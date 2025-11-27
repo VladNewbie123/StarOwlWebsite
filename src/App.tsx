43ce76e1ab803877264b47bf5d714aa00853dc77
VladNewbie123 <guglmoj67@gmail.com>
2025-11-27 18:52:23 +0200

    update translations and fix types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer"
-import { Language } from "@/data/translations";
+import type { Language } from "@/data/translations";
 import Home from "@/pages/Home";
 import Projects from "@/pages/Projects";
 import {Routes, Route} from "react-router-dom";
